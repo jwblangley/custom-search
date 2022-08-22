ab89e6e7affbc3259d8ce9a3ca01d41e60c1fe40
James Langley <jwblangley@gmail.com>
2022-08-22 21:12:10 +0100

    Implement MVP

---
+++ config.js
@@ -0,0 +1,11 @@
+const debug = true;
+
+const placeholder = "%s";
+
+const fallback = "https://www.google.com/search?q=%s";
+
+const config = {
+  test: "hello%s",
+};
+
+export { debug, placeholder, fallback, config };
